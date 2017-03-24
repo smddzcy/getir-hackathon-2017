1eb4af89555e14f9d52a035224218619159518d8
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-03-25 01:56:15 +0300

    Map info adapter changed

--- android/app/src/main/java/com/wow/wowmeet/screens/main/map/MapInfoWindowAdapter.java
+++ android/app/src/main/java/com/wow/wowmeet/screens/main/map/MapInfoWindowAdapter.java
@@ -1,14 +1,14 @@
 package com.wow.wowmeet.screens.main.map;
 
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.wow.wowmeet.R;
 import com.wow.wowmeet.models.Event;
-
-import java.util.ArrayList;
 
 /**
  * Created by mahmutkaraca on 3/25/17.
@@ -16,22 +16,25 @@ import java.util.ArrayList;
 
 public class MapInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
-    private Context context;
-    private ArrayList<Event> events;
+    private View infoView;
 
-
-    public MapInfoWindowAdapter(Context context, ArrayList<Event> dataSet) {
-        this.context = context;
-        this.events = dataSet;
+    public MapInfoWindowAdapter(View infoView) {
+        this.infoView = infoView;
     }
 
     @Override
     public View getInfoWindow(Marker marker) {
-        LayoutInflater inflater = LayoutInflater.from(context);
+        ImageView imgProfile = (ImageView) infoView.findViewById(R.id.imgMapProfilePicture);
+        TextView txtUsername = (TextView) infoView.findViewById(R.id.txtMapUsername);
+        TextView txtActivityType = (TextView) infoView.findViewById(R.id.txtActivityType);
+        TextView txtActivityTime = (TextView) infoView.findViewById(R.id.txtActivityTime);
+        Button btnGo = (Button) infoView.findViewById(R.id.btnGo);
 
-        
+        Event e = (Event) marker.getTag();
 
-        return null;
+
+
+        return infoView;
     }
 
     @Override

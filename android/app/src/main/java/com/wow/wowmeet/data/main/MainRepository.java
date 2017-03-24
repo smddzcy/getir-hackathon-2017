c287feb5b729c30e4775bc689a358314e237b8a8
Ergün Erdoğmuş <ergunerdogmus@Ergun-MacBook-Pro.local>
2017-03-24 22:53:48 +0300

    UserModel and its Builder is created

---
+++ android/app/src/main/java/com/wow/wowmeet/data/main/MainRepository.java
@@ -0,0 +1,9 @@
+package com.wow.wowmeet.data.main;
+
+/**
+ * Created by ergunerdogmus on 24.03.2017.
+ */
+
+public interface MainRepository {
+
+}
---
+++ android/app/src/main/java/com/wow/wowmeet/models/User.java
@@ -0,0 +1,66 @@
+package com.wow.wowmeet.models;
+
+/**
+ * Created by ergunerdogmus on 24.03.2017.
+ */
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    class UserBuilder {
+        private String username;
+        private String email;
+        private String password;
+
+        public UserBuilder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public User createUser() {
+            return new User(username, email, password);
+        }
+    }
+
+    User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

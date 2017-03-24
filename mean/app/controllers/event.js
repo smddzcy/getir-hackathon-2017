186a971ff9ee5fc67131d9c6a54e940ef2f099be
Samed Duzcay <samedduzcay@gmail.com>
2017-03-25 02:39:13 +0300

    Wrap event create success message in an object

--- mean/app/controllers/event.js
+++ mean/app/controllers/event.js
@@ -4,7 +4,7 @@ angular.module('MyApp')
       Event.send($scope.event)
         .then(function(response) {
           $scope.messages = {
-            success: ["Event has been created successfully."]
+            success: [{ msg:"Event has been created successfully." }]
           };
         })
         .catch(function(response) {

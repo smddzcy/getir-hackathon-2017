b01a98a399038fece3ba98b4feffb321c979f113
Samed Duzcay <samedduzcay@gmail.com>
2017-03-25 00:12:03 +0300

    Changing whole architecture from full stack node app to node backend API & Angular frontend

---
+++ mean/controllers/contact.js
@@ -0,0 +1,45 @@
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
+var transporter = nodemailer.createTransport(mg({
+  auth: {
+    api_key: process.env.MAILGUN_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  }
+}));
+
+/**
+ * GET /contact
+ */
+exports.contactGet = function(req, res) {
+  res.render('contact', {
+    title: 'Contact'
+  });
+};
+
+/**
+ * POST /contact
+ */
+exports.contactPost = function(req, res) {
+  req.assert('name', 'Name cannot be blank').notEmpty();
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('email', 'Email cannot be blank').notEmpty();
+  req.assert('message', 'Message cannot be blank').notEmpty();
+  req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    return res.status(400).send(errors);
+  }
+
+  var mailOptions = {
+    from: req.body.name + ' ' + '<'+ req.body.email + '>',
+    to: 'your@email.com',
+    subject: '✔ Contact Form | Mega Boilerplate',
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, function(err) {
+    res.send({ msg: 'Thank you! Your feedback has been submitted.' });
+  });
+};

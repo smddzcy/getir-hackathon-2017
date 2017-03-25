d1df15caaea9ea8188444edb000590553b6ec494
Ergün Erdoğmuş <ergunerdogmus@Ergun-MacBook-Pro.local>
2017-03-25 13:47:03 +0300

    MapView small typo fix2

--- android/app/src/main/java/com/wow/wowmeet/screens/main/map/MapFragment.java
+++ android/app/src/main/java/com/wow/wowmeet/screens/main/map/MapFragment.java
@@ -90,7 +90,7 @@ public class MapFragment extends SupportMapFragment implements MapContract.View 
         if(mapReady && map != null) {
             for (Event e : events) {
                 map.addMarker(new MarkerOptions()
-                    .position(new LatLng(e.getLocation().getLatitude(), e.getLocation().getLongtitude())))
+                    .position(new LatLng(e.getLocation().getLatitude(), e.getLocation().getLongitude())))
                     .setTag(e);
             }
         } else {

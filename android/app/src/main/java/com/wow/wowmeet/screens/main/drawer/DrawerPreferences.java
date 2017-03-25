b6efccd21c28c758a20c3412d0b3a3db6390d351
Ergün Erdoğmuş <ergunerdogmus@Ergun-MacBook-Pro.local>
2017-03-25 12:22:25 +0300

    DrawerLogout implemented

---
+++ android/app/src/main/java/com/wow/wowmeet/screens/main/drawer/DrawerPreferences.java
@@ -0,0 +1,9 @@
+package com.wow.wowmeet.screens.main.drawer;
+
+/**
+ * Created by ergunerdogmus on 25.03.2017.
+ */
+
+public interface DrawerPreferences {
+    void removeUserPrefs();
+}
--- android/app/src/main/java/com/wow/wowmeet/screens/main/drawer/DrawerPresenter.java
+++ android/app/src/main/java/com/wow/wowmeet/screens/main/drawer/DrawerPresenter.java
@@ -7,9 +7,11 @@ package com.wow.wowmeet.screens.main.drawer;
 public class DrawerPresenter implements DrawerContract.Presenter {
 
     private DrawerContract.View view;
+    private DrawerPreferences drawerPreferences;
 
-    public DrawerPresenter(DrawerContract.View view) {
+    public DrawerPresenter(DrawerContract.View view, DrawerPreferences drawerPreferences) {
         this.view = view;
+        this.drawerPreferences = drawerPreferences;
     }
 
     @Override
@@ -24,6 +26,7 @@ public class DrawerPresenter implements DrawerContract.Presenter {
 
     @Override
     public void onItemClick(int position, String action) {
-
+        drawerPreferences.removeUserPrefs();
+        view.goLogin();
     }
 }

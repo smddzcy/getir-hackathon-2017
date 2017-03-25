b31fc381e87ef2167c4eaed179f429608aff51df
Ergün Erdoğmuş <ergunerdogmus@Ergun-MacBook-Pro.local>
2017-03-25 14:52:30 +0300

    EventInfoActivity back button now works

--- android/app/src/main/java/com/wow/wowmeet/screens/eventinfo/EventInfoActivity.java
+++ android/app/src/main/java/com/wow/wowmeet/screens/eventinfo/EventInfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -62,6 +63,17 @@ public class EventInfoActivity extends AppCompatActivity implements EventInfoCon
     @Override
     public void showError(String e) {
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override

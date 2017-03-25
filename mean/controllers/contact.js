b4bd186492c10a878e79a22230e9aec9366f4fa9
Samed Duzcay <samedduzcay@gmail.com>
2017-03-25 05:06:13 +0300

    Fix visual bugs

--- mean/controllers/contact.js
+++ mean/controllers/contact.js
@@ -34,8 +34,8 @@ exports.contactPost = function(req, res) {
 
   var mailOptions = {
     from: req.body.name + ' ' + '<'+ req.body.email + '>',
-    to: 'your@email.com',
-    subject: '✔ Contact Form | Mega Boilerplate',
+    to: 'wowteam.smddzcy@mailhero.io',
+    subject: '✔ WowMeet Contact Form',
     text: req.body.message
   };
 
--- mean/controllers/user.js
+++ mean/controllers/user.js
@@ -226,7 +226,7 @@ exports.forgotPost = function(req, res, next) {
       var mailOptions = {
         to: user.email,
         from: 'support@yourdomain.com',
-        subject: '✔ Reset your password on Mega Boilerplate',
+        subject: '✔ Reset your password on WowMeet',
         text: 'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n' +
         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
         'http://' + req.headers.host + '/reset/' + token + '\n\n' +
@@ -279,7 +279,7 @@ exports.resetPost = function(req, res, next) {
       var mailOptions = {
         from: 'support@yourdomain.com',
         to: user.email,
-        subject: 'Your Mega Boilerplate password has been changed',
+        subject: 'Your WowMeet password has been changed',
         text: 'Hello,\n\n' +
         'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
       };

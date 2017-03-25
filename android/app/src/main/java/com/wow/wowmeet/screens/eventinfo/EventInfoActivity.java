3c35ca0264af219e212a9457db5e3be1dffce9e8
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-03-25 13:18:52 +0300

    Added EventInfoActivity

---
+++ android/app/src/main/java/com/wow/wowmeet/screens/eventinfo/EventInfoActivity.java
@@ -0,0 +1,15 @@
+package com.wow.wowmeet.screens.eventinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wow.wowmeet.R;
+
+public class EventInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_info);
+    }
+}

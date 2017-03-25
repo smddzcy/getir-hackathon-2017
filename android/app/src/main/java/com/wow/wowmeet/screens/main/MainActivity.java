5f06dbb80e8bae594a6523e5ba5266c8b417a236
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-03-25 11:31:56 +0300

    Added DrawerLayout

--- android/app/src/main/java/com/wow/wowmeet/screens/main/MainActivity.java
+++ android/app/src/main/java/com/wow/wowmeet/screens/main/MainActivity.java
@@ -1,12 +1,20 @@
 package com.wow.wowmeet.screens.main;
 
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.wow.wowmeet.R;
 import com.wow.wowmeet.base.BaseActivity;
+import com.wow.wowmeet.models.User;
+import com.wow.wowmeet.screens.login.LoginActivity;
+import com.wow.wowmeet.screens.main.drawer.DrawerFragment;
+import com.wow.wowmeet.utils.Constants;
 
 import java.util.ArrayList;
 
@@ -19,12 +27,20 @@ public class MainActivity extends BaseActivity implements MainContract.View {
 
     @BindView(R.id.viewPager) ViewPager viewPager;
     MainPagerAdapter mainPagerAdapter;
+    @BindView(R.id.drawerLayout) DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Intent i = getIntent();
+        User user = (User) i.getSerializableExtra(Constants.INTENT_EXTRA_USER);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.drawerContainer, DrawerFragment.newInstance(user), "DRAWER")
+                .commit();
 
         MainPresenter presenter = new MainPresenter(this);
         setPresenter(presenter);
@@ -53,5 +69,14 @@ public class MainActivity extends BaseActivity implements MainContract.View {
     @Override
     public void refreshListAndMap(ArrayList arr) {
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ android/app/src/main/java/com/wow/wowmeet/screens/main/drawer/DrawerFragment.java
@@ -0,0 +1,66 @@
+package com.wow.wowmeet.screens.main.drawer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.wow.wowmeet.R;
+import com.wow.wowmeet.models.User;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DrawerFragment extends Fragment {
+
+    public static final String ARG_USER = "com.wow.wowmeet.MainActivity.DrawerFragment.User";
+
+    private User user;
+
+    public static DrawerFragment newInstance(User user) {
+        DrawerFragment fragment = new DrawerFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_USER, user);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public DrawerFragment() {}
+
+    @BindView(R.id.txtUsernameDrawer) TextView txtUsername;
+    @BindView(R.id.imgDrawerProfilePicture) ImageView imgProfile;
+    @BindView(R.id.frameUserBackground) FrameLayout userBackgroundFrame;
+    @BindView(R.id.listDrawerOptions) ListView drawerOptions;
+
+    private ArrayAdapter<String> drawerOptionsArrayAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        user = (User) getArguments().getSerializable(ARG_USER);
+        View v = inflater.inflate(R.layout.fragment_drawer, container, false);
+        ButterKnife.bind(this, v);
+
+        txtUsername.setText(user.getName());
+
+        String[] options = getResources().getStringArray(R.array.drawer_options_array);
+
+        drawerOptionsArrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, options);
+        drawerOptions.setAdapter(drawerOptionsArrayAdapter);
+
+
+        return v;
+    }
+
+}

385adc94472e6c6143d0d633c776dd0cce375629
Ergün Erdoğmuş <ergunerdogmus@Ergun-MacBook-Pro.local>
2017-03-25 16:48:35 +0300

    ChatFragment recyclerview bound to its adapter

--- android/app/src/main/java/com/wow/wowmeet/partials/chat/ChatFragment.java
+++ android/app/src/main/java/com/wow/wowmeet/partials/chat/ChatFragment.java
@@ -3,12 +3,16 @@ package com.wow.wowmeet.partials.chat;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.wow.wowmeet.R;
+import com.wow.wowmeet.models.Message;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,6 +38,10 @@ public class ChatFragment extends Fragment {
     @BindView(R.id.fragment_chat_rv)
     RecyclerView chatRecyclerView;
 
+    ChatListAdapter chatListAdapter;
+
+    private ArrayList<Message> messages;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +53,12 @@ public class ChatFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
         ButterKnife.bind(this, rootView);
+
+        messages = new ArrayList<>();
+        chatListAdapter = new ChatListAdapter(messages);
+        chatRecyclerView.setAdapter(chatListAdapter);
+        chatRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         return rootView;
     }
 

8e6887715a7e5e3965996eb5f02fa7ecd4b3f346
Ergün Erdoğmuş <ergunerdogmus@Ergun-MacBook-Pro.local>
2017-03-26 12:13:01 +0300

    CreateEventPresenter fix

--- android/app/src/main/java/com/wow/wowmeet/screens/createevent/CreateEventPresenter.java
+++ android/app/src/main/java/com/wow/wowmeet/screens/createevent/CreateEventPresenter.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
@@ -12,6 +11,7 @@ import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.model.LatLng;
 import com.wow.wowmeet.R;
+import com.wow.wowmeet.data.EventTypeRepository;
 import com.wow.wowmeet.data.createevent.CreateEventRepository;
 import com.wow.wowmeet.models.Event;
 import com.wow.wowmeet.models.Location;
@@ -19,7 +19,6 @@ import com.wow.wowmeet.models.Type;
 import com.wow.wowmeet.partials.dialogs.DatePickerFragment;
 import com.wow.wowmeet.partials.dialogs.TimePickerFragment;
 import com.wow.wowmeet.utils.CalendarUtils;
-import com.wow.wowmeet.utils.DialogHelper;
 
 import java.util.Calendar;
 import java.util.List;
@@ -44,6 +43,7 @@ public class CreateEventPresenter implements CreateEventContract.Presenter {
     private Place pickedPlace;
 
     private CreateEventRepository createEventRepository;
+    private EventTypeRepository eventTypeRepository;
 
     private DisposableSingleObserver<List<Type>> disposableSingleEventTypesObserver;
     private DisposableSingleObserver<String> disposableSingleCreateEventObserver;
@@ -81,12 +81,13 @@ public class CreateEventPresenter implements CreateEventContract.Presenter {
         this.dateStartTimePickerReference = Calendar.getInstance();
         this.dateEndTimePickerReference = Calendar.getInstance();
         this.createEventRepository = new CreateEventRepository();
+        this.eventTypeRepository = new EventTypeRepository();
     }
 
 
     @Override
     public void start() {
-        Single<List<Type>> singleTypes = createEventRepository.getTypes();
+        Single<List<Type>> singleTypes = eventTypeRepository.getTypes();
 
         disposableSingleEventTypesObserver = singleTypes.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
